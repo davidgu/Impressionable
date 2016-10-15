8e8f577c45ec73823693ebe373613796370324b9
David Gu <david.gu@hotmail.com>
2016-10-15 03:46:01 -0400

    Fixed compilation issues

--- Impressionable-Android/app/src/main/java/impressionable/impressionable_android/VotingPage.java
+++ Impressionable-Android/app/src/main/java/impressionable/impressionable_android/VotingPage.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 
 public class VotingPage extends AppCompatActivity {
